a00a6cb7f87b745700c77bdbb2b22b464041948f
Nelasov <alexey.n@thewhitelabelagency.com>
2021-04-30 12:40:53 +0300

    add section, title, text no styles

---
+++ test-project.php
@@ -0,0 +1,12 @@
+<?php ?>
+
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="title">test title</h2>
+                <p class="text">test description</p>
+            </div>
+        </div>
+    </div>
+</div>
